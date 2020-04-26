1064f494c1a090f5ed28346bc70da77e3e6eb115
sarthak-choudhary <f20190112@pilani.bits-pilani.ac.in>
2020-04-26 15:56:49 +0530

    django secret key removed

--- Zwinger/settings.py
+++ Zwinger/settings.py
@@ -20,7 +20,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'm%znakybl2ci$h!9%p&l^y2&1jyvfkmn+r^lvkaz&1x^fge&%p'
+SECRET_KEY = 'secret'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
